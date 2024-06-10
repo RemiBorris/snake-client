42be6b1ba6ef89c5052db4973b0c6b58f04df965
remiborris <remiborris@gmail.com>
2024-06-10 19:52:59 -0300

    create constants.js file to hold all constants of snek game such as key inputs, ports and IP

---
+++ constants.js
@@ -0,0 +1,17 @@
+const IP = "localhost";
+const PORT = "50541";
+const keyPresses = {
+  w: "Move: up",
+  a: "Move: left",
+  s: "Move: down",
+  d: "Move: right",
+  l: "Say: try harder",
+  o: "Say: This ones mine",
+  p:"Say: Nice Try!",
+};
+
+module.exports = {
+  IP,
+  PORT,
+  keyPresses,
+};
