9dc1c2e63ee7ac6410174b030bebd3ad671b18b9
remiborris <remiborris@gmail.com>
2024-06-10 16:38:14 -0300

    play around with moving the snek with setimeout functions

--- client.js
+++ client.js
@@ -14,6 +14,15 @@ const connect = function() {
     console.log("Successfully connected to game server");
     conn.write("Name: REM");
   });
+  conn.on("connect", () => {
+    // setInterval(() => {
+    //   conn.write("Move: up");
+    // }, 100);
+    // setTimeout(() => conn.write("Move: up"), 1000);
+    // setTimeout(() => conn.write("Move: up"), 2000);
+    // setTimeout(() => conn.write("Move: up"), 3000);
+    // setTimeout(() => conn.write("Move: up"), 4000);
+  });
   return conn;
 };
 
