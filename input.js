1acbee889db7220481c2b7f5844977be1c5f6471
remiborris <remiborris@gmail.com>
2024-06-10 18:50:35 -0300

    add code to connect input to the client so that it can send data to game server

--- input.js
+++ input.js
@@ -1,9 +1,12 @@
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
   const stdin = process.stdin;  // create variable to hold the stdin object so we don't have to type process.stdin multiple times
   stdin.setRawMode(true); // Raw Mode allows us to listen for individual keypresses instead of waiting for the user to press enter
   stdin.setEncoding("utf8"); // utf8 encoding is set so that we can read the text data that is input
   stdin.resume(); // resume stdin so the program can listen for input
   stdin.on("data", handleUserInput);
+  connection = conn;
   return stdin;   // return the stdin object so we can use it elsewhere in the program
 };
 
@@ -11,9 +14,22 @@ const handleUserInput = function(key) {
   if (key === "\u0003") {
     process.exit();
   }
+  if (key === "w") {
+    connection.write("Move: up");
+  }
+  if (key === "a") {
+    connection.write("Move: left");
+  }
+  if (key === "s") {
+    connection.write("Move: down");
+  }
+  if (key === "d") {
+    connection.write("Move: right");
+  }
 };
+
 const input = {
   setupInput,
 };
 
-module.exports = setupInput;
+module.exports = input;
